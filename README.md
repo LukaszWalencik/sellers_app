f01f9979e3f7a312d3d5137eaa9dca4d986fb3c7
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-02-25 16:41:40 +0100

    Start food app project

---
+++ README.md
@@ -0,0 +1,3 @@
+# sellers_app
+
+A new Flutter project.
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sellers App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(),
+    );
+  }
+}
