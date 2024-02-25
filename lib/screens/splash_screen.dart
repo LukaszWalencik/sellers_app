8a54eec8cfc8f802a98bb215e657dfb6bd29ab03
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-02-25 17:47:49 +0100

    Add some simple body to SplashScreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -10,6 +10,29 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.white,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/splash.jpg'),
+            const SizedBox(height: 10),
+            const Padding(
+              padding: EdgeInsets.all(18.0),
+              child: Text(
+                'Sell Food Online',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 40,
+                    fontFamily: 'Signatra',
+                    letterSpacing: 3),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
