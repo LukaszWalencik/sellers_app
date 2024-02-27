eef6e5381c48ca4abfd35c6351ef6d6bf563d386
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-02-27 20:13:16 +0100

    Add whole body of RegisterScreen

--- lib/screens/authentication/registration/register_screen.dart
+++ lib/screens/authentication/registration/register_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:sellers_app/widgets/custom_text_field.dart';
 
 class RegisterScreen extends StatefulWidget {
@@ -9,26 +12,129 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  TextEditingController nameController = TextEditingController();
+
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController loactionController = TextEditingController();
+  XFile? imageXFile;
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        CustomTextField(
-          controller: emailController,
-          icon: Icons.email,
-          hintText: 'Email',
-          isObscure: false,
+    final double width = MediaQuery.of(context).size.width;
+    return SingleChildScrollView(
+      child: Container(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            const SizedBox(height: 10),
+            InkWell(
+              child: CircleAvatar(
+                radius: width * 0.15,
+                backgroundColor: Colors.white,
+                backgroundImage: imageXFile == null
+                    ? null
+                    : FileImage(
+                        File(imageXFile!.path),
+                      ),
+                child: imageXFile == null
+                    ? Icon(
+                        Icons.add_photo_alternate,
+                        size: width * 0.15,
+                        color: Colors.grey,
+                      )
+                    : null,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  CustomTextField(
+                    icon: Icons.person,
+                    controller: nameController,
+                    hintText: 'Name',
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    icon: Icons.email,
+                    controller: emailController,
+                    hintText: 'Email',
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    icon: Icons.lock,
+                    controller: passwordController,
+                    hintText: 'Password',
+                    isObscure: true,
+                  ),
+                  CustomTextField(
+                    icon: Icons.lock,
+                    controller: confirmPasswordController,
+                    hintText: 'Confirm Password',
+                    isObscure: true,
+                  ),
+                  CustomTextField(
+                    icon: Icons.phone,
+                    controller: phoneController,
+                    hintText: 'Phone',
+                    isObscure: false,
+                  ),
+                  CustomTextField(
+                    icon: Icons.my_location,
+                    controller: loactionController,
+                    hintText: 'Cafe/Restaurant Adress',
+                    isObscure: false,
+                    enable: false,
+                  ),
+                  Container(
+                    width: 400,
+                    height: 40,
+                    alignment: Alignment.center,
+                    child: ElevatedButton.icon(
+                      onPressed: () => print('Get location'),
+                      icon: const Icon(
+                        Icons.location_on,
+                        color: Colors.white,
+                      ),
+                      label: const Text(
+                        'Get my current location',
+                        style: TextStyle(color: Colors.green),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  ElevatedButton(
+                    onPressed: () => print('Sign Up'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green,
+                    ),
+                    child: const Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            )
+          ],
         ),
-        CustomTextField(
-          controller: passwordController,
-          icon: Icons.key,
-          hintText: 'Email',
-          isObscure: false,
-        ),
-      ],
+      ),
     );
   }
 }
