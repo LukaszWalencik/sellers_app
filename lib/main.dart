50f301a3ffa3d1ef963fb889722ecc0a007cb8b4
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-02-29 11:46:34 +0100

    Add initializing Firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:sellers_app/screens/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   WidgetsFlutterBinding.ensureInitialized;
   runApp(const MainApp());
 }
