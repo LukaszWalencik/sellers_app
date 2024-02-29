170851d56efce940eca52f664f7308dcf02e2a9b
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-02-29 21:21:36 +0100

    Create LoadingDialog

---
+++ lib/widgets/loading_dialog.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:sellers_app/widgets/progress_bar.dart';
+
+class LoadingDialog extends StatelessWidget {
+  final String? message;
+  const LoadingDialog({
+    Key? key,
+    this.message,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      key: key,
+      content: Column(
+        children: [
+          circularProgress(),
+          const SizedBox(height: 10),
+          Text(message! + 'Please wait..')
+        ],
+      ),
+    );
+  }
+}
