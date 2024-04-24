60b6c8fa44cead8affd15feb7fa1c1809dee8c33
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-04-24 22:48:03 +0200

    Create empty widget named MenusUploadScreen

---
+++ lib/screens/menus_upload_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MenusUploadScreen extends StatefulWidget {
+  const MenusUploadScreen({super.key});
+
+  @override
+  State<MenusUploadScreen> createState() => _MenusUploadScreenState();
+}
+
+class _MenusUploadScreenState extends State<MenusUploadScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
