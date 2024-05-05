26f9eb895dbf4f2c3ef80a4e832257db1974e35f
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-05-05 22:25:01 +0200

    Copy InfoDesign from users_app and add some changes to use it in sellers app

---
+++ lib/widgets/info_design.dart
@@ -0,0 +1,66 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+import 'package:sellers_app/model/menus_model.dart';
+
+class InfoDesign extends StatefulWidget {
+  MenusModel? menusModel;
+  BuildContext? context;
+  InfoDesign({
+    super.key,
+    this.menusModel,
+  });
+
+  @override
+  State<InfoDesign> createState() => _InfoDesignState();
+}
+
+class _InfoDesignState extends State<InfoDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Colors.green,
+      child: Padding(
+        padding: const EdgeInsets.all(5),
+        child: SizedBox(
+          height: 285,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            children: [
+              Divider(
+                height: 4,
+                thickness: 3,
+                color: Colors.grey[300],
+              ),
+              Image.network(
+                widget.menusModel!.thumbnailUrl!,
+                height: 220,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 1),
+              Text(
+                widget.menusModel!.menuTitle!,
+                style: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 20,
+                  fontFamily: 'Train',
+                ),
+              ),
+              Text(
+                widget.menusModel!.menuInfo!,
+                style: const TextStyle(
+                  color: Colors.blue,
+                  fontSize: 12,
+                ),
+              ),
+              const Divider(
+                height: 4,
+                thickness: 3,
+                color: Colors.grey,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
