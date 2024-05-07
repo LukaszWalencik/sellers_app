dda1a57abde28a159852753434dbd03fdf3e6832
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-05-07 21:13:16 +0200

    Add body to ItemsScreen

--- lib/screens/items_screen.dart
+++ lib/screens/items_screen.dart
@@ -1,9 +1,15 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import 'package:sellers_app/model/menus_model.dart';
+import 'package:sellers_app/screens/global/global.dart';
+import 'package:sellers_app/screens/menus_upload_screen.dart';
+
 class ItemsScreen extends StatefulWidget {
+  final MenusModel? menusModel;
   const ItemsScreen({
     Key? key,
+    this.menusModel,
   }) : super(key: key);
 
   @override
@@ -13,6 +19,38 @@ class ItemsScreen extends StatefulWidget {
 class _ItemsScreenState extends State<ItemsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Colors.blue,
+                Colors.green,
+              ],
+              begin: FractionalOffset(0.0, 0.0),
+              end: FractionalOffset(1.0, 0.0),
+              stops: [0.0, 1.0],
+              tileMode: TileMode.clamp,
+            ),
+          ),
+        ),
+        title: Text(
+          sharedPreferences!.getString('sellerName')!,
+          style: const TextStyle(fontSize: 30, fontFamily: 'Lobster'),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const MenusUploadScreen()));
+              },
+              icon: const Icon(Icons.post_add))
+        ],
+      ),
+    );
   }
 }
