397b3aac4eac8faf1b648ea9ad7411df72644283
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-05-07 21:08:50 +0200

    Create empty ItemScreen

---
+++ lib/screens/items_screen.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+class ItemsScreen extends StatefulWidget {
+  const ItemsScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<ItemsScreen> createState() => _ItemsScreenState();
+}
+
+class _ItemsScreenState extends State<ItemsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
