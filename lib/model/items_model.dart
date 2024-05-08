ab5d5463ff8762f7e64f609c5b95cd2497c5ede2
Lukasz Walencik <walencik.lukasz@gmail.com>
2024-05-08 20:21:54 +0200

    add toJson and fromJson to decode and encode data from snapshot

--- lib/model/items_model.dart
+++ lib/model/items_model.dart
@@ -26,4 +26,43 @@ class ItemsModel {
     this.status,
     this.thumbnailUrl,
   });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'itemID': itemID,
+      'menuID': menuID,
+      'sellerUID': sellerUID,
+      'itemInfo': itemInfo,
+      'itemTitle': itemTitle,
+      'description': description,
+      'price': price,
+      'publishedDate': publishedDate,
+      'status': status,
+      'thumbnailUrl': thumbnailUrl,
+    };
+  }
+
+  factory ItemsModel.fromMap(Map<String, dynamic> map) {
+    return ItemsModel(
+      itemID: map['itemID'] != null ? map['itemID'] as String : null,
+      menuID: map['menuID'] != null ? map['menuID'] as String : null,
+      sellerUID: map['sellerUID'] != null ? map['sellerUID'] as String : null,
+      itemInfo: map['itemInfo'] != null ? map['itemInfo'] as String : null,
+      itemTitle: map['itemTitle'] != null ? map['itemTitle'] as String : null,
+      description:
+          map['description'] != null ? map['description'] as String : null,
+      price: map['price'] != null ? map['price'] as int : null,
+      publishedDate: map['publishedDate'] != null
+          ? map['publishedDate'] as Timestamp
+          : null,
+      status: map['status'] != null ? map['status'] as String : null,
+      thumbnailUrl:
+          map['thumbnailUrl'] != null ? map['thumbnailUrl'] as String : null,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ItemsModel.fromJson(String source) =>
+      ItemsModel.fromMap(json.decode(source) as Map<String, dynamic>);
 }
